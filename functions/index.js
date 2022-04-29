378bd88616ae0043f64d2163bb39bd2056a68818
denis.danyliuk <denis.danyliuk@requestum.com>
2022-04-29 11:49:37 +0300

    Add sendMessageListenerPushNotification core

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,9 @@
 const functions = require("firebase-functions");
-
 const admin = require('firebase-admin');
+
 admin.initializeApp();
 const db = admin.firestore();
+
 
 exports.userChanged = functions.firestore
     .document('users/{userId}')
@@ -39,4 +40,46 @@ exports.userChanged = functions.firestore
             .catch((error) => {
                 console.log("Error getting documents: ", error);
             });
+    });
+
+exports.sendMessageListenerPushNotification = functions.firestore
+    .document('chatsMessages/{chatId}/messages/{messageId}')
+    .onCreate((snap, context) => {
+        const message = snap.data();
+        console.log("chatId", context.params.chatId);
+        console.log("message", message);
+
+        // const messageId = context.params.messageId;
+
+        const sentBy = message.sentBy;
+
+        return db.collection("chats")
+            .doc(context.params.chatId.toString())
+            .get()
+            .then((snap) => {
+                var chatData = snap.data();
+                var sender = (chatData.user1.uid == sentBy) ? chatData.user1 : chatData.user2
+                var recepient = (chatData.user1.uid == sentBy) ? chatData.user2 : chatData.user1
+                
+                const payload = {
+                    token: recepient.fcmToken,
+                    notification: {
+                        title: sender.firstName,
+                        body: message.messageText
+                    },
+                    data: {
+                        body: message.messageText,
+                    }
+                };
+                console.log("chatData", chatData);
+                console.log("sender", sender);
+                console.log("recepient", recepient);
+                console.log("payload", payload);
+
+                return admin.messaging().send(payload).then((response) => {
+                    console.log('Successfully sent message:', response);
+                }).catch((error) => {
+                    console.log("Error sending push: ", error);
+                });
+            })
     });
